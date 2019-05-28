1bcca690fd5015df39d5e12f9266338cd43e952d
Evgeny Zemtsov <eugene.zemtsov@gmail.com>
2019-05-28 13:06:10 +0200

    new functions for grid

--- web/src/index.js
+++ web/src/index.js
@@ -1,47 +1,83 @@
-window.onload = function() {
-  document.body.appendChild(grid);
-};
-
-let size = 20;
-
+const size = 3;
 let turn = 0;
+
 let lastClicked;
 
-let grid = clickableGrid(size, function(el, row, col) {
-  console.log("You clicked on element:", el);
-  console.log("You clicked on row:", row);
-  console.log("You clicked on col:", col);
+const isEven = (n) => (n % 2 == 0);
 
-  if (isEven(turn)) { el.innerHTML = 'X'; } else {
-    el.innerHTML = 'O';
+let selectCell = (grid, row, col) => {
+  let cells = grid.getElementsByTagName('td');
+  let cellIndex = col + row * size;
+  let cell = cells[cellIndex];
+
+  if (cell !== lastClicked) {
+    if (lastClicked)
+      lastClicked.classList
+        .replace('clicked', 'normal');
+    lastClicked = cell;
+
+    if (isEven(turn)) { cell.innerHTML = 'X'; }
+    else { cell.innerHTML = 'O'; }
+    cell.classList.toggle('clicked');
+
+    turn++;
   }
-  el.className = 'clicked';
+};
 
-  if (lastClicked) lastClicked.className = '';
-  lastClicked = el;
-  turn++;
-});
-
-
-function clickableGrid(size, callback) {
-  let grid = document.createElement('table');
-  grid.className = 'grid';
+let generateGrid = (size) => {
+  var grid = document.createElement('table');
+  grid.classList.toggle('grid');
 
   for (let r = 0; r < size; ++r) {
-    let tr = grid.appendChild(document.createElement('tr'));
+    let tr = grid
+      .appendChild(document.createElement('tr'));
     for (let c = 0; c < size; ++c) {
-      let cell = tr.appendChild(document.createElement('td'));
-      cell.innerHTML = " ";
-      cell.addEventListener('click', (function(el, r, c) {
-        return function() {
-          callback(el, r, c);
-        };
-      })(cell, r, c), false);
+      let cell = tr
+        .appendChild(document.createElement('td'));
+      cell.onclick = () => {
+        console.log("Clicked on cell:", { r, c });
+        selectCell(grid, r, c);
+      };
+    };
+  };
+  return grid;
+};
+
+let fillGrid = (grid) => {
+  let cells = grid.getElementsByTagName('td');
+  for (let [r, rv] of Object.entries(testData)) {
+    for (let [c, cv] of Object.entries(rv)) {
+      let cellIndex = Number(c) + Number(r) * size;
+      cells[cellIndex].innerHTML = cv;
     };
   }
-  return grid;
-}
+};
 
-function isEven(n) {
-  return (n % 2 == 0);
-}
+window.onload = () => {
+  let grid = generateGrid(size);
+  document.body.appendChild(grid);
+
+  fillGrid(grid);
+  selectCell(grid, 2, 2);
+};
+
+const testData = {
+  0: {
+    0: "",
+    1: "X",
+    2: "",
+  },
+  1: {
+    0: "",
+    1: "O",
+    2: "",
+  },
+  2: {
+    0: "",
+    1: "X",
+    2: "",
+  }
+};
+
+
+
