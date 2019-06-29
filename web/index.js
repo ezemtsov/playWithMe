e110317867822cc737fe6bfe06cf870bef872eb9
Evgeny Zemtsov <eugene.zemtsov@gmail.com>
2019-06-29 13:43:16 +0200

    Dev -> Prod

--- web/index.js
+++ web/index.js
@@ -66,8 +66,8 @@ class Game {
   }
   connect(name) {
     let myGame = this;
-    //let socket = new WebSocket('ws://34.68.64.169:8080');
-    let socket = new WebSocket('ws://0.0.0.0:8080');
+    let socket = new WebSocket('ws://34.68.64.169:8080');
+    //let socket = new WebSocket('ws://0.0.0.0:8080');
 
     socket.onopen = function(event) {
       console.log('Connected to: ' + event.currentTarget.url);
